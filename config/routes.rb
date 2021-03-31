a3425b11f9c96cb40af84813e1d53acf4be4e35b
Amara Cole <amara.cole@yahoo.com>
2021-03-31 16:45:54 +0200

    new routes for bookings

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :bookings, only: [:create]
   end
 
-  # resources :bookings, only: [:index, :show, :update, :destroy]
+  resources :bookings, only: [:index, :show, :update, :destroy]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
