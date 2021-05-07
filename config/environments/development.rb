78d4037eb06151d69a28dc387e84220a4e5d11f4
Amara Cole <amara.cole@yahoo.com>
2021-05-07 19:09:54 +0200

    removed the browser confign file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,4 +61,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.hosts << /[a-z0-9]+\.ngrok\.io/
+  config.hosts << "892452f73397.ngrok.io"
+
 end
